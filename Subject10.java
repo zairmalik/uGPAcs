d56d653250438998cdba5434d57cdba00c83bf9a
Uzair <31562068+zairmalik@users.noreply.github.com>
2018-11-06 19:22:25 +0800

    Create Subject10.java

---
+++ Subject10.java
@@ -0,0 +1,13 @@
+package com.learn.uzair.ugpacs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Subject10 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject10);
+    }
+}
